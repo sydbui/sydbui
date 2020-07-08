943f9e8eb9d0d89ee01179802d8ca7a826480c25
Sydney Bui <sydbui@berkeley.edu>
2020-07-08 01:18:47 -0700

    update site

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ import {
   Image,
   Text,
 } from "@chakra-ui/core";
-import {MdArrowDownward} from "react-icons/md";
+import {BsArrowDown} from "react-icons/bs";
 import InfoBox from '../components/infobox';
 import * as styles from '../components/customtext';
 
@@ -66,12 +66,14 @@ export default function Home() {
               </Text> She is also very cool.
             </Text>
           </Flex>
-          <Flex justifyContent="space-between" width="100%" marginTop="90px">
-          <Text fontFamily="Inconsolata" color="#FFCC7E" fontSize="28px">
-            <Link href="mailto:sydneythibui@gmail.com">Email</Link> *  <Link href="/resume.pdf" isExternal>
-            Resume</Link> * <Link href="https://www.linkedin.com/in/sydney-bui/" isExternal>LinkedIn</Link>
-          </Text>
-            <Box as={MdArrowDownward} size="100px" color="#FFCC7E"></Box>
+          <Flex justifyContent="space-between" width="100%">
+            <Flex marginTop="90px">
+              <Text fontFamily="Inconsolata" color="#FFCC7E" fontSize="28px">
+              <Link href="mailto:sydneythibui@gmail.com">Email</Link> *  <Link href="/resume.pdf" isExternal>
+              Resume</Link> * <Link href="https://www.linkedin.com/in/sydney-bui/" isExternal>LinkedIn</Link>
+              </Text>
+            </Flex>
+            <Box as={BsArrowDown} size="150px" color="#FFCC7E"></Box>
           </Flex>
           <Box height="192px" width="100%"></Box>
           <InfoBox title="1951 Coffee Trainee App" subtitle="SOFTWARE ENGINEER" body="Through Blueprint, a student organization 
@@ -83,21 +85,19 @@ export default function Home() {
           limited cooking knowledge to easily find a variety of new recipes through user research and design." img="/pics/ponyo.png" link="/swag" check={false}></InfoBox>
           <Box height="192px" width="100%"></Box>
         </Flex>
-        <Flex height="488px" width="100%" backgroundColor="#FFCC7E">
-          <Flex marginTop="88px" marginLeft="126px" marginLeft="126px" width="950px" flexDirection="column">
+        <Flex width="100%" backgroundColor="#FFCC7E">
+          <Flex marginTop="88px" marginLeft="126px" marginLeft="126px" width="950px" flexDirection="column" marginBottom="90px">
             <styles.Title color="#424242" text="Want to work with me?"></styles.Title>
             <Box height="36px" width="100%"></Box>
             <styles.Subtitle color="#424242" text="I am actively looking for internships and ready 
             to start summer 2021. If you’d like to talk to me over a game of sudoku, get in touch!"></styles.Subtitle>
             <Box height="36px" width="100%"></Box>
-            <Text fontFamily="Inconsolata" color="#424242" fontSize="28px">
+            <Text fontFamily="Inconsolata" color="#424242" fontSize="28px" fontWeight="700">
             <Link href="mailto:sydneythibui@gmail.com">Email</Link>  *   <Link href="/resume.pdf" isExternal>
             Resume</Link>  *  <Link href="https://www.linkedin.com/in/sydney-bui/" isExternal>LinkedIn</Link>
             </Text>
           </Flex>
-          <Flex width = "320px" justifyContent="center" flexDirection="column">
-            <Image marginLeft="45px" rounded="full" size="225px" src="/pics/me!.jpg"/>
-          </Flex>
+          <Image marginLeft="45px" rounded="full" size="225px" src="/pics/me!.jpg" display="flex" marginTop="130px" marginRight="100px"/>
         </Flex>
       </ThemeProvider>
     </div>
--- pages/swag.js
+++ pages/swag.js
@@ -6,17 +6,12 @@ import {
   Flex, 
   Box, 
   Link, 
-  Button, 
-  Icon, 
-  ButtonGroup,   
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
+  Text,
   Tooltip,
   Image
 } from "@chakra-ui/core";
 import {MdArrowDownward} from "react-icons/md";
-import * as styles from '../styles/styles';
+import * as styles from '../components/customtext';
 import HoverText from '../components/hoverfunc';
 import InfoBox from '../components/infobox';
 
@@ -43,42 +38,135 @@ export default function Home() {
         <title>syd's site</title>
         <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:wght@500&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@700&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet"></link>
       </Head>
 
       <ThemeProvider >
         <CSSReset config={config} />
-        <styles.Main>
-          <Flex>
-            <Tooltip label="Return Home" placement="right" backgroundColor="transparent" color="#C273E7" 
+        <Flex background-color= "transparent" marginLeft="125px" marginRight="126px" flexWrap="wrap" flexDirection="row" marginTop="80px">
+          <Tooltip label="Return Home" placement="right" backgroundColor="transparent" color="#C273E7" 
             shadow="none" fontFamily="Zilla Slab" fontSize="28px">
-                <styles.Header fontFamily="Zilla Slab" color="#C273E7" height="77px">
-                  <Link href="/" >*</Link>
-                </styles.Header>
-            </Tooltip>
-          </Flex>
+            <Flex>
+              <styles.Header color="#C273E7" text={<Link href="/" >*</Link>}></styles.Header>
+            </Flex>
+          </Tooltip>
           <Box width="100%" height="30px"></Box>
-          <styles.Header fontFamily="Zilla Slab" fontSize="48px">RecipeBook iOS App</styles.Header>
-          <styles.SubHeader fontFamily="Inconsolata" color="#B54EE5">PRODUCT DESIGN | 2 WEEKS</styles.SubHeader>
+          <styles.Title text="RecipeBook iOS App"></styles.Title>
+          <Box width="100%"></Box>
+          <styles.Subtitle color="#B54EE5" text="PRODUCT DESIGN | 2 WEEKS"></styles.Subtitle>
           <Box height="36px" width="100%"></Box>
-          <Flex width="100%" marginBottom="24px">
-            <styles.BodyText fontFamily="Inconsolata" color="#424242">hello</styles.BodyText>
+          <styles.Subtitle color="#424242" text="RecipeBook is an iOS App I designed to allow users to easily find a 
+          variety of dishes to cook. The skills used include UI Design, UX reseach, and Visual Design"></styles.Subtitle>
+          <Box height="50px" width="100%"></Box>
+          <styles.Subtitle color="#B54EE5" text="USER RESEARCH TACTICS"></styles.Subtitle>
+          <Box height="24px" width="100%"></Box>
+          <styles.Subtitle color="#424242" text="Through conducting multiple interviews with siblings and 
+          friends with skills ranging from vast cooking experience to none, I was able to learn about their 
+          experiences with cooking. I was also able to identify potential problem areas and key insights through 
+          additional reading and research."></styles.Subtitle>
+          <Box height="60px" width="100%"></Box>
+          <styles.Subtitle color="#B54EE5" text="KEY TAKE AWAYS"></styles.Subtitle>
+          <Box height="24px" width="100%"></Box>
+          <styles.Underline color="#424242" text="TIME CONSTRAINTS"></styles.Underline>
+          <styles.Subtitle color="#424242" text="Along with yummy recipes and dishes, people 
+          prefer for cooking to be quick and time efficient (typically 20 mins)"></styles.Subtitle>
+          <Box width="100%" height="15px"></Box>
+          <styles.Underline color="#424242" text="HEALTH CONCERNS"></styles.Underline>
+          <styles.Subtitle color="#424242" text="People prefer healthy recipes as they are more 
+          conscious of what they eat when cooking as they see all ingredients that go into the dish."></styles.Subtitle>
+          <Box width="100%" height="15px"></Box>
+          <styles.Underline color="#424242" text="VARIETY"></styles.Underline>
+          <styles.Subtitle color="#424242" text="When it comes to cooking, people feel restricted by both their skill set 
+          and options of what they know how to cook. RecipeBook’s goal is to recommend new and welcoming recipes to widen 
+          one’s skill."></styles.Subtitle>
+          <Box width="100%" height="60px"></Box>
+          <styles.Subtitle color="#B54EE5" text="THE PROBLEM"></styles.Subtitle>
+          <Box height="24px" width="100%"></Box>
+          <styles.Underline color="#424242" text="CUSTOMIZATION"></styles.Underline>
+          <styles.Subtitle color="#424242" text="Each person has specific dietary preferences and restrictions that 
+          make it difficult to chose what to cook."></styles.Subtitle>
+          <Box width="100%" height="60px"></Box>
+          <styles.Subtitle color="#B54EE5" text="SKETCHES & MOCK-UPS"></styles.Subtitle>
+          <Image src="/pics/sketches.jpg" marginTop="24px" ></Image>
+          <Box width="35px"></Box>
+          <styles.Caption color="#424242" text="Sketches for Home and Search tab and information on recipe 
+          categories."></styles.Caption>
+          <Box width="100%" height="60px"></Box>
+          <styles.Subtitle color="#B54EE5" text="PRODUCT STRUCTURE"></styles.Subtitle>
+          <Image src="/pics/structure.png" marginTop="24px" marginLeft="20px" marginBottom="60px"></Image>
+          <styles.Subtitle color="#B54EE5" text="DESIGN DECISION: USER ACCESSIBILITY"></styles.Subtitle>
+          <Box height="24px" width="100%"></Box>
+          <styles.Subtitle color="#424242" text="For the search tab, I weighed the pros and cons of having a 3-column 
+          grid or a 2-column grid to showcase the categories of recipes. Taking into consideration user accessibility 
+          and effective visual design, the 2-column grid presents a clearer, simpler, and more accessible display of 
+          the categories."></styles.Subtitle>
+          <Image src="/pics/comp.png" marginTop="24px"  marginBottom="60px"></Image>
+          <styles.Subtitle color="#B54EE5" text="VISUAL DESIGN: COLOR PALETTE"></styles.Subtitle>
+          <Box height="24px" width="100%"></Box>
+          <Flex width="100%" height="100%">
+            <Flex width="775px">
+              <styles.Subtitle color="#424242" text="The color palette I chose highlights orange and yellow tones because 
+              research shows that orange and yellow colors make people hungry which is ideal when looking for a recipe to 
+              cook, increasing user usage and engagement"></styles.Subtitle>
+            </Flex>
+            <Image src="/pics/colors.png" marginTop="24px" marginLeft="20px" marginBottom="60px" size="30%"></Image>
           </Flex>
-        </styles.Main>
-        <Flex height="488px" width="100%" backgroundColor="#FFCC7E">
-          <Flex marginTop="88px" marginLeft="126px" marginLeft="126px" width="950px" flexDirection="column">
-            <styles.Header fontFamily="Zilla Slab" color="#424242" fontSize="48px">Want to work with me?</styles.Header>
+          <styles.Subtitle color="#B54EE5" text="PROTOTYPES & HI FIDELITY MOCKUPS"></styles.Subtitle>
+          <Box height="24px" width="100%"></Box>
+          <Flex flexDirection="row" width="100%" justifyContent="space-between">
+            <Flex width="100%" justifyContent="center" flexWrap="wrap">
+              <styles.Underline color="#424242" text="BROWSING RECIPES"></styles.Underline>
+              <Image src="/pics/browsing.gif" marginTop="20px" marginBottom="20px"></Image>
+            </Flex>
+            <Flex width="20%"></Flex>
+            <Flex width="100%" justifyContent="center" flexWrap="wrap">
+              <styles.Underline color="#424242" text="SEARCH RECIPES"></styles.Underline>
+              <Image src="/pics/search.gif" marginTop="20px"marginBottom="20px"></Image>
+            </Flex>
+            <Flex width="20%"></Flex>
+            <Flex width="100%" justifyContent="center" flexWrap="wrap">
+              <styles.Underline color="#424242" text="SAVING RECIPES"></styles.Underline>
+              <Image src="/pics/saved.gif" marginTop="20px" marginBottom="20px"></Image>
+            </Flex>
+          </Flex>
+          <Flex flexDirection="row" width="100%" justifyContent="space-between" marginBottom="70px">
+            <Flex width="100%" justifyContent="center" flexWrap="wrap">
+              <styles.Caption color="#424242" text="Users can scroll through recommended categories, and the recipe previews 
+              allow users to see cooking time, difficulty, servings, and ratings."></styles.Caption>
+            </Flex>
+            <Flex width="20%"></Flex>
+            <Flex width="100%" justifyContent="center" flexWrap="wrap">
+              <styles.Caption color="#424242" text="Upon clicking on a category, users will be taken 
+              to a new page with countless fitting recipes to choose from as well as addtional information 
+              about them."></styles.Caption>
+            </Flex>
+            <Flex width="20%"></Flex>
+            <Flex width="100%" justifyContent="center" flexWrap="wrap">
+              <styles.Caption color="#424242" text="If a user wants to save a recipe they can simply press the black 
+              outlined bookmark on the top right corner of the recipe. The bookmark will then be filled and 
+              the saved recipes can be accessed through the “Saved” tab."></styles.Caption>
+            </Flex>
+          </Flex>
+          <styles.Subtitle color="#B54EE5" text="MOVING FORWARD"></styles.Subtitle>
+          <Box height="24px" width="100%"></Box>
+          <styles.Subtitle color="#424242" text="This project allowed me to explore user design interactions on digital platforms. 
+          Moving forward, this project is still a work in progress and I intend to conduct further research to maximize user 
+          experience and interaction"></styles.Subtitle>
+          <Box height="200px" width="100%"></Box>
+        </Flex>
+        <Flex width="100%" backgroundColor="#FFCC7E">
+          <Flex marginTop="88px" marginLeft="126px" marginLeft="126px" width="950px" flexDirection="column" marginBottom="90px">
+            <styles.Title color="#424242" text="Want to work with me?"></styles.Title>
             <Box height="36px" width="100%"></Box>
-            <styles.BodyText fontFamily="Inconsolata" color="gray.700">I am actively looking for internships and ready 
-            to start summer 2021. If you’d like to talk to me over a game of sudoku, get in touch!</styles.BodyText>
+            <styles.Subtitle color="#424242" text="I am actively looking for internships and ready 
+            to start summer 2021. If you’d like to talk to me over a game of sudoku, get in touch!"></styles.Subtitle>
             <Box height="36px" width="100%"></Box>
-            <styles.Links fontFamily="Inconsolata" color="gray.700">
+            <Text fontFamily="Inconsolata" color="#424242" fontSize="28px" fontWeight="700">
             <Link href="mailto:sydneythibui@gmail.com">Email</Link>  *   <Link href="/resume.pdf" isExternal>
             Resume</Link>  *  <Link href="https://www.linkedin.com/in/sydney-bui/" isExternal>LinkedIn</Link>
-            </styles.Links>
+            </Text>
           </Flex>
-          <Flex width = "320px" justifyContent="center" flexDirection="column">
-            <Image marginLeft="45px" rounded="full" size="225px" src="/pics/ponyo.png"/>
-          </Flex>
+          <Image marginLeft="45px" rounded="full" size="225px" src="/pics/me!.jpg" display="flex" marginTop="130px" marginRight="100px"/>
         </Flex>
       </ThemeProvider>
     </div>
